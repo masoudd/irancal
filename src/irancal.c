8030bdbed5629abda310370be091a23aa5fefb4f
Masoud Naservand <masoud.naservand@gmail.com>
2024-02-29 09:31:37 +0330

    Add GPL3 license

--- src/irancal.c
+++ src/irancal.c
@@ -1,6 +1,25 @@
+/*
+	Copyright (C) 2024  Masoud Naservand
+
+	This file is part of irancal.
+
+	irancal is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	irancal is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with irancal.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #ifndef UNICODE
 #define UNICODE
-#endif 
+#endif
 #define _POSIX_C_SOURCE 1
 
 #include <windows.h>
